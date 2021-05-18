e2c5274160808239e1c5304999196c5d2b86dd3f
wozijisun <wozijisun@sina.com>
2021-05-18 17:47:31 +0800

    关于相关函数的修改

--- color.py
+++ color.py
@@ -14,22 +14,23 @@ def color(src, dst):
 
   graph = tf.Graph()
   with graph.as_default():
-    with tf.gfile.FastGFile(src, 'rb') as f:
+    with tf.io.gfile.GFile(src, 'rb') as f:
       image_data = f.read()
       input_image = tf.image.decode_jpeg(image_data, channels=3)
-      input_image = tf.image.resize_images(input_image, size=(IMG_SIZE, IMG_SIZE))
+      input_image = tf.image.resize(input_image, size=(IMG_SIZE, IMG_SIZE))
       input_image = utils.convert2float(input_image)
       input_image.set_shape([IMG_SIZE, IMG_SIZE, 3])
 
-    with tf.gfile.FastGFile(MODEL, 'rb') as model_file:
-      graph_def = tf.GraphDef()
+    with tf.io.gfile.GFile(MODEL, 'rb') as model_file:
+      # graph_def = tf.Graph()
+      graph_def = tf.compat.v1.GraphDef()
       graph_def.ParseFromString(model_file.read())
     [output_image] = tf.import_graph_def(graph_def,
                           input_map={'input_image': input_image},
                           return_elements=['output_image:0'],
                           name='output')
 
-  with tf.Session(graph=graph) as sess:
+  with tf.compat.v1.Session(graph=graph) as sess:
     generated = output_image.eval()
     with open(dst, 'wb') as f:
       f.write(generated)
